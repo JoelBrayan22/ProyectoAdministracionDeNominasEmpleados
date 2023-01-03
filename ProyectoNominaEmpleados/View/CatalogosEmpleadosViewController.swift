19efab1069118bb2696f3fab52c9d3d48b0304ba
joelbrayan22 <joelnavorjimenez@gmail.com>
2023-01-03 16:54:33 -0600

    Commit StoryBoard and Views

---
+++ ProyectoNominaEmpleados/View/CatalogosEmpleadosViewController.swift
@@ -0,0 +1,29 @@
+//
+//  CatalogosEmpleadosViewController.swift
+//  ProyectoNominaEmpleados
+//
+//  Created by MacBook on 03/01/23.
+//
+
+import UIKit
+
+class CatalogosEmpleadosViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ ProyectoNominaEmpleados/View/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ProyectoNominaEmpleados
+//
+//  Created by MacBook on 03/01/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
