0e783da040bfbb2615bf7e0c060fb60351c02c3d
MacBook <Macbook@MacBook-Pro-de-MACBOOK.local>
2023-01-05 17:06:17 -0600

    Actualizacion Controller y Pantalla Login

--- ProyectoNominaEmpleados/Controllers/NominaController.swift
+++ ProyectoNominaEmpleados/Controllers/NominaController.swift
@@ -36,64 +36,133 @@ class NominaController {
     // Funciones
     func empleadoLogin(correo: String, password: String) {
         
+        if let empleado = self.model.empleadoLogIn(correo: correo, password: password) {
+            self.viewControllerDelegate?.empleado(empleadoLogin: empleado)
+        } else {
+            self.viewControllerDelegate?.empleado(empleadoLoginError: "Error al iniciar sesión con \(correo)")
+        }
+        
     }
     
     // Obtener todos los empleados existentes
     func getEmpleados () {
         
-        self.model.getEmpleados()
-        catalogoEmpleadosDelegate?.empleado(empleadosCargados: self.model.empleados)
+        let empleados = self.model.getEmpleados()
+        
+        catalogoEmpleadosDelegate?.empleado(empleadosCargados: empleados)
         
     }
     
     // Crear un empleado nuevo en AddEmpleadoVC
     func addEmpleado(id: Int, nombre: String, area: String, departamento: String, puesto: String, fechaContratacion: Date, antiguedad: Int, salario: Double, fechaVacacionesInicio: Date, fechaVacacionesFin: Date, estaVacaciones: Bool, tienePrestamo: Bool) {
         
-        if let empleado = self.model.addEmpleado(nombre: nombre, area: area, departamento: departamento, puesto: puesto, salario: salario, fechaContratacion: fechaContratacion) {
+        if let empleado = self.model.addEmpleado(id: Int(Int32.random(in: 1...Int32.max)), nombre: nombre, area: area, departamento: departamento, puesto: puesto, fechaContratacion: fechaContratacion, salario: salario) {
             
             addEmpleadoDelegate?.empleado(empleadoCreado: empleado)
             catalogoEmpleadosDelegate?.empleado(empleadoCreado: empleado)
             
         }
+        // TODO: como seleccionar la fechaContratacion
     }
     
-    func seleccionarFechaContratacion() {
+    func seleccionarTipoFechaComoContratacion() {
         
-        if let fecha = model.fechaSeleccionada {
-            addEmpleadoDelegate?.empleado(fechaContratado: fecha)
-        }
+        self.model.tipoFecha = .fechaContratacion
+        
+    }
+    
+    func seleccionarTipoFechaComoInicioVacaciones() {
+        
+        self.model.tipoFecha = .inicioVacaciones
+        
+    }
+    
+    func seleccionarTipoFechaComoFinVacaciones() {
+        
+        self.model.tipoFecha = .finVacaciones
+        
+    }
+    
+    func seleccionarTipoFechaComoFechaPago() {
+        
+        self.model.tipoFecha = .fechaPago
         
     }
     
     // Empleo seleccionado en CatalogoEmpleadosVC
     func seleccionarEmpleado(index: Int, empleado: EmpleadoEntity) {
+        
         if let empleado = model.seleccionarEmpleado(index: index, empleado: empleado) {
+            
             catalogoEmpleadosDelegate?.empleado(empleadoSeleccionado: empleado, index: index)
         }
-        
     }
     
     // Empleado seleccionado que necesita DetallesEmpleadoVC para mostrarlo
     func getEmpleadoSeleccionado() {
-        detallesEmpleadoDelegate?.empleado(empleadoSeleccionado: model.empleadoSeleccionado)
+        if let empleadoSeleccionado = self.model.empleadoSeleccionado {
+            detallesEmpleadoDelegate?.empleado(empleadoSeleccionado: empleadoSeleccionado)
+        }
     }
     
+    // Obtener todo el historial de pagos del empleado
     func obtenerHistorialPagos() {
-        historialNominaDelegate?.salario(historialSalario: <#T##<<error type>>#>)
+        
+        historialNominaDelegate?.pago(historialPagos: self.model.pagos.filter({
+            pago in
+            pago.empleado == self.model.empleadoSeleccionado
+        }))
+        
     }
     
     // Selecciona una fecha
-    func seleccionarFecha(fechaSeleccionada: Date, tipoFecha: TipoFecha) {
-        
+    func seleccionarFecha(fechaSeleccionada: Date) {
+        switch self.model.tipoFecha {
+        case .fechaContratacion:
+            self.model.fechaContratacion = fechaSeleccionada
+            self.addEmpleadoDelegate?.empleado(fechaContratado: fechaSeleccionada)
+        case .inicioVacaciones:
+            self.model.fechaInicioVacaciones = fechaSeleccionada
+            self.seleccionarVacacionesDelegate?.empleado(fechaSeleccionada: fechaSeleccionada, tipoFecha: .inicioVacaciones)
+        case .finVacaciones:
+            self.model.fechaFinVacaciones = fechaSeleccionada
+            self.seleccionarVacacionesDelegate?.empleado(fechaSeleccionada: fechaSeleccionada, tipoFecha: .inicioVacaciones)
+        case .fechaPago:
+            self.model.fechaPago = fechaSeleccionada
+            self.addPagoDelegate?.salario(fechaPago: fechaSeleccionada, tipoFech: .fechaPago)
+        default:
+            print("Tipo de fecha no válido")
+        }
     }
 
     
-
-    func seleccionarPago(index: Int) {
+    // Selecciona un pago desde HistorialNominaVC
+    func seleccionarPago(index: Int, pago: PagoEntity) {
+        
+        if let pago = model.seleccionarPago(index: index, pago: pago) {
+            
+            historialNominaDelegate?.pago(pagoSeleccionado: pago, index: index)
+        }
         
     }
+    
+    // Pago seleccionado que necesita DetallePagoVC
+    func getPagoSeleccionado() {
+        if let pagoSeleccionado = self.model.pagoSeleccionado {
+            detallePagoDelegate?.salario(pagoSeleccionado: pagoSeleccionado)
+        }
+    }
 
-    func agregarPago(nombreEmpleado: String, fechaPago: Date, sueldo: Double, viaticos: Double?, prestamo: Double?, descripcionPrestamo: String?, cantidadRestantePrestamo: Double?, numeroAbono: Int?) {
+    // Generar un nuevo pago
+    func agregarPago(fechaPago: Date, sueldo: Double, viaticos: Double?, prestamo: Double?, descripcionPrestamo: String?, cantidadRestantePrestamo: Double?, numeroAbono: Int?) {
         
+        if let pago = self.model.agregarPago(nombreEmpleado: self.model.empleadoSeleccionado?.nombre ?? "DESCONOCIDO", fechaPago: fechaPago, sueldo: sueldo, viaticos: viaticos, prestamo: prestamo, descripcionPrestamo: descripcionPrestamo, cantidadRestantePrestamo: cantidadRestantePrestamo, numeroAbono: numeroAbono) {
+            
+            addPagoDelegate?.salario(salarioCreado: pago)
+            
+        } else {
+            addPagoDelegate?.salario(salarioCreadoError: "No se pudo crear el pago.")
+        }
     }
 }
+
--- ProyectoNominaEmpleados/ViewController.swift
+++ ProyectoNominaEmpleados/ViewController.swift
@@ -18,10 +18,33 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        NominaController.shared.viewControllerDelegate = self
     }
 
     @IBAction func loginActionButton(_ sender: Any) {
+        
+        guard let correo = usuarioTextField.text else { return }
+        guard let contraseña = contraseñaTextField.text else { return }
+        
+        NominaController.shared.empleadoLogin(correo: correo, password: contraseña)
+        
     }
+    
+}
+
+extension ViewController: ViewControllerDelegate {
+    func empleado(empleadoLogin empleado: EmpleadoEntity) {
+        self.performSegue(withIdentifier: "GoCatalogoEmpleadosVC", sender: nil)
+    }
+    
+    func empleado(empleadoLoginError message: String) {
+        let alert = UIAlertController(title: "Error", message: message, preferredStyle: .alert)
+        
+        alert.addAction(UIAlertAction(title: "ok", style: .default))
+        
+        self.present(alert, animated: true)
+    }
+    
     
 }
 
